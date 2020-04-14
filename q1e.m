3eba75e96fc07919b3a70a2999b06070de6022a9
iSach <sacha.lewin@me.com>
2020-04-14 04:53:36 +0200

    Fixed 1de

--- q1e.m
+++ q1e.m
@@ -1,104 +1,111 @@
-function time = q1e()
+function [hands_prob, time, expectation, variance] = q1e(colors_nbr)
 % Question 1(e).
 % 
-% Calcule et génère pour un nombre de couleurs de 4 à 10 le temps
+% Calcule et génère pour un nombre de couleurs entre 4 et 10 le temps
 % nécessaire pour calculer les probabilités, l'espérance et la variance.
 %
 % Sortie :
 %       time : Vecteur de longueur 10 avec les temps, en secondes
 %              pour chacun des nombres de couleurs. time(1:3) = -1 car
 %              on travaille avec 4 couleurs minimum.
-
-    time = repmat(-1, 10, 1);
+%       expectation : l'espérance
+%       variance : la variance
     
-    for nbr = 9:9
-       tic;
-       calc_values(nbr);
-       time(nbr) = toc;
-    end
+    tic;
     
-    function [hands_prob, expectation, variance] = ...
-            calc_values(colors_nbr)
-    % Calcule les probabilités, l'espérance et la variance
-    % pour un jeu de colors_nbr couleurs au total.
-    % Où colors_nbr est entre 4 et 20.
-    
-        % Couleurs du jeu de base.
-        std_colors = 'RBVN';
-        % 16 couleurs éventuelles ajoutées en plus, pour avoir jusque 20.
-        % On choisit arbitrairement des autres lettres pour
-        % représenter chaque nouvelle couleur.
-        add_colors = 'ACDEFGHIJKLMOPQT';
+    % Couleurs du jeu de base.
+    std_colors = 'RBVN';
+    % 16 couleurs éventuelles ajoutées en plus, pour avoir jusque 20.
+    % On choisit arbitrairement des autres lettres pour
+    % représenter chaque nouvelle couleur.
+    add_colors = 'ACDEFGHIJKLMOPQT';
 
-        % On ajoute colors_nbr - 4 couleurs, puisqu'on a déjà 4 de base.
-        colors_to_add = colors_nbr - 4;
+    % On ajoute colors_nbr - 4 couleurs, puisqu'on a déjà 4 de base.
+    colors_to_add = colors_nbr - 4;
 
-        % Selon le nombre de couleurs à ajouter spécifié par colors_nbr
-        % On crée un vecteur contenant nos couleurs au total.
-        chosen_colors = [std_colors, add_colors(1:colors_to_add)];
+    % Selon le nombre de couleurs à ajouter spécifié par colors_nbr
+    % On crée un vecteur contenant nos couleurs au total.
+    chosen_colors = [std_colors, add_colors(1:colors_to_add)];
 
-        urn = gen_urn(chosen_colors, 5);
+    urn = gen_urn(chosen_colors, 5);
 
-        % Compteur de mains obtenues.
-        % 1 : couleur
-        % 2 : carré
-        % 3 : full
-        % 4 : brelan
-        % 5 : double paire
-        % 6 : simple paire.
-        % 7 : Famille
-        counter = zeros(7, 1);
+    % Compteur de mains obtenues.
+    % 1 : couleur
+    % 2 : carré
+    % 3 : full
+    % 4 : brelan
+    % 5 : double paire
+    % 6 : simple paire.
+    % 7 : Famille
+    counter = zeros(7, 1);
 
-        tree = create_naive_tree(urn, 5, false);
-        events_nbr = length(tree);
+    tree = create_naive_tree(urn, 5, false);
+    events_nbr = length(tree);
 
-        for curr = 1:events_nbr
-            hand = tree(curr, :);
-            gain = calc_gain(hand, chosen_colors);
-
-            switch(gain)
-                case 1000
-                    counter(1) = counter(1) + 1;
-                case 700
-                    counter(2) = counter(2) + 1;
-                case 450
-                    counter(3) = counter(3) + 1;
-                case 200
-                    counter(4) = counter(4) + 1;
-                case 75
-                    counter(5) = counter(5) + 1;
-                case 0
-                    counter(6) = counter(6) + 1;
-                case 80
-                    counter(7) = counter(7) + 1;
+    for curr = 1:events_nbr
+        % Calculer nombre d'occurences de chaque couleur dans la main.
+        color_count = zeros(colors_nbr, 1);
+        for i = 1:5
+            ball = tree(curr, i);
+            for j=1:colors_nbr
+                if chosen_colors(j) == ball
+                    color_count(j) = color_count(j) +  1;
+                end
             end
         end
 
-        % Les évènements étant équiprobables, la probabilité pour chaque main
-        % est donnée en divisant le nombre d'occurences de l'évènements par
-        % le nombre total d'évènements.
-        hands_prob = 1 / events_nbr .* counter;
+        % Trie par ordre décroissant pour identifier le nombre le + grand.
+        color_count = sort(color_count, 'descend');
 
-        % E(X) l'espérance
-        expectation = hands_prob(6) * 0 + ...
-                      hands_prob(5) * 75 + ...
-                      hands_prob(7) * 80 + ...
-                      hands_prob(4) * 200 + ...
-                      hands_prob(3) * 450 + ...
-                      hands_prob(2) * 700 + ...
-                      hands_prob(1) * 1000;
-        % E(X^2)       
-        expectation_2 = hands_prob(6) * 0 + ...
-                      hands_prob(5) * 75 * 75 + ...
-                      hands_prob(7) * 80 * 80 + ...
-                      hands_prob(4) * 200 * 200 + ...
-                      hands_prob(3) * 450 * 450 + ...
-                      hands_prob(2) * 700 * 700 + ...
-                      hands_prob(1) * 1000 * 1000;
-
-        % Formule de Konig-Huygens : Var(x) = E(X^2) - (E(X))^2  
-        variance = expectation_2 - expectation^2;
+        % Comparaison et identification de chaque cas.
+        switch color_count(1)
+            case 5 % Couleur
+                counter(1) = counter(1) + 1;
+            case 4 % Carré
+                counter(2) = counter(2) + 1;
+            case 3 % Full ou Brelan
+                if color_count(2) == 2 % 3 et 2 -> Full
+                    counter(3) = counter(3) + 1;
+                else
+                    counter(4) = counter(4) + 1;
+                end
+            case 2
+                if color_count(2) == 2 % 2 et 2 -> Double Paire
+                    counter(5) = counter(5) + 1;
+                else
+                   counter(6) = counter(6) + 1;
+                end
+            case 1
+                counter(7) = counter(7) + 1;
+        end
     end
+
+    % Les évènements étant équiprobables, la probabilité pour chaque main
+    % est donnée en divisant le nombre d'occurences de l'évènements par
+    % le nombre total d'évènements.
+    hands_prob = 1 / events_nbr .* counter;
+
+    % E(X) l'espérance
+    expectation = hands_prob(6) * 0 + ...
+                  hands_prob(5) * 75 + ...
+                  hands_prob(7) * 80 + ...
+                  hands_prob(4) * 200 + ...
+                  hands_prob(3) * 450 + ...
+                  hands_prob(2) * 700 + ...
+                  hands_prob(1) * 1000;
+    % E(X^2)       
+    expectation_2 = hands_prob(6) * 0 + ...
+                  hands_prob(5) * 75 * 75 + ...
+                  hands_prob(7) * 80 * 80 + ...
+                  hands_prob(4) * 200 * 200 + ...
+                  hands_prob(3) * 450 * 450 + ...
+                  hands_prob(2) * 700 * 700 + ...
+                  hands_prob(1) * 1000 * 1000;
+              
+    % Formule de Konig-Huygens : Var(x) = E(X^2) - (E(X))^2  
+    variance = expectation_2 - expectation^2;
+    
+    time = toc;
       
     function urn = gen_urn(colors, balls_per_color)
     % Génère une urne avec balls_per_color fois chaque couleur.
@@ -106,69 +113,10 @@ function time = q1e()
         colors = colors(:);
         total_colors = size(colors, 1);
         urn = blanks(total_colors * balls_per_color)';
-        for j = 1:total_colors
+        for col = 1:total_colors
             for k = 1:balls_per_color
-                urn((j-1) * balls_per_color + k) = colors(j);
+                urn((col-1) * balls_per_color + k) = colors(col);
             end
-        end
-    end   
-
-    function gain = calc_gain(hand, colors)
-    % Calcule le gain en fonction de la main obtenue.
-    %
-    % Supporte la famille comme main en plus.
-    %
-    % Paire   -> 0
-    % 2-Paire -> 75
-    % Famille -> 80
-    % Brelan  -> 200
-    % Full    -> 450
-    % Carré   -> 700
-    % Couleur -> 1000
-    %
-    % Exemple : calc_gain('RBVVV', colors) -> 200 (Brelan)
-    
-        hand = hand(:);
-        colors = colors(:);
-    
-        % Calculer nombre d'occurences de chaque couleur dans la main.
-        color_nbr = zeros(size(colors, 1), 1);
-        for i = 1:size(hand, 1)
-            index = find(colors == hand(i));
-            color_nbr(index) = ...
-                color_nbr(index) +  1;
-        end
-        
-        % Trie par ordre décroissant pour identifier le nombre le + grand.
-        color_nbr = sort(color_nbr, 'descend');
-        
-        % Comparaison et identification de chaque cas.
-        switch color_nbr(1)
-            case 5 % Couleur
-                gain = 1000;
-                return
-            case 4 % Carré
-                gain = 700;
-                return
-            case 3 % Full ou Brelan
-                if color_nbr(2) == 2 % 3 et 2 -> Full
-                    gain = 450;
-                    return
-                end
-
-                gain = 200; % Brelan sinon.
-                return
-            case 2
-                if color_nbr(2) == 2 % 2 et 2 -> Double Paire
-                    gain = 75;
-                    return
-                end
-
-                gain = 0; % Paire simple sinon.
-                return
-            case 1
-                gain = 80;
-                return
         end
     end
 end
---
+++ q1e_basil.m
@@ -0,0 +1,172 @@
+%Ce script va nous permettre de calculer la probabilité de chaque main
+%en fonction du nombre de couleurs présentes dans l'urne ainsi que
+%l'espérance et la variance correspondante.
+tic
+
+%On veut commencer par créer notre tableau. Pour ne pas devoir modifier
+%tout le code à chaque fois, on va partir de tab_colors pour générer notre
+%tree. On ne devra modifier que tab_colors pour ajouter ou retirer des
+%couleurs.
+tab_colors = ['R' 'B' 'V' 'N' 'D' 'A' 'Z' 'E' 'R'];
+taille_colors = length(tab_colors);
+balls_in_urn = zeros(1, 5*taille_colors);
+
+for i=1 : taille_colors 
+    for  j=1 : 5
+        d = 5*(i-1) + j;
+        balls_in_urn(d) = tab_colors(i);
+    end
+end
+
+balls_in_urn = char(balls_in_urn);
+
+%Création de l'arbre
+nb_draws = 5;
+replacement = false;
+
+tree = create_naive_tree(balls_in_urn,nb_draws,replacement);
+
+taille = length(tree);
+
+%On initialise les compteurs
+
+nb_simple_paire = 0;
+nb_double_paire = 0;
+nb_famille = 0;
+nb_brelan = 0;
+nb_full = 0;
+nb_carre = 0;
+nb_couleur = 0;
+
+%On va ensuite procéder comme à la q1d, en prenant en compte que le nombre
+%de couleur vaut taille_colors et qu'on doit compter le nombre de familles
+
+%On va analyser chaque main l'une après l'autre
+for i=1 : taille    
+    %On va générer un vecteur tab_nb_couleurs qui compte le nb de
+    %répétitions de chaque couleur et sera de type [ nb_R nb_B nb_V nb_N ]
+    tab_nb_couleurs = zeros(1,taille_colors);
+    for k=1 : nb_draws
+        for j=1 : taille_colors
+            if tree(i,k) == tab_colors(j)
+                tab_nb_couleurs(j) = tab_nb_couleurs(j) + 1;
+                break
+            end
+        end
+    end
+    
+    %Maintenant qu'on a notre tab_nb_couleurs, on va déterminer quel type
+    %de main nous analysons et incrémenter le compteur correspondant
+    for j=1 : taille_colors
+        
+        %Si on tombe sur un 5 ou un 4, pas de doute possible, c'est une
+        %couleur ou un carré. On sort directement de la boucle après avoir
+        %incrémenté le compteur correspondant
+        if tab_nb_couleurs(j) == 5
+            nb_couleur = nb_couleur + 1;
+            break
+        end
+        
+        if tab_nb_couleurs(j) == 4
+            nb_carre = nb_carre + 1;
+            break
+        end
+        
+        %Si on tombe sur un 3, c'est qu'on a affaire soit à un brelan soit
+        %un full. On teste si on a un 2 autre part dans le vecteur. Si
+        %c'est le cas, on a un full, sinon on a un brelan
+        if tab_nb_couleurs(j) == 3
+            for d=1 : taille_colors
+                if tab_nb_couleurs(d) == 2
+                    nb_full = nb_full + 1;
+                    break
+                end
+            end
+            if tab_nb_couleurs(d) == 2
+                break
+            end
+            nb_brelan = nb_brelan + 1;
+            break
+        end
+        
+        %Si on tombe sur un 2, c'est qu'on a affaire soit à une paire
+        %simple, une paire double ou un full.
+        if tab_nb_couleurs(j) == 2
+            %on teste si c'est un full
+            for d=1 : taille_colors
+                if tab_nb_couleurs(d) == 3
+                    nb_full = nb_full + 1;
+                    break
+                end
+            end
+            if tab_nb_couleurs(d) == 3
+                break
+            end
+            %on teste mtn si c'est une double paire
+            for d=1 : taille_colors
+                if j ~= d
+                    if tab_nb_couleurs(d) == 2
+                        nb_double_paire = nb_double_paire + 1;
+                        break
+                    end
+                end
+            end
+            if j ~= d 
+                if tab_nb_couleurs(d) == 2
+                    break
+                end
+            end
+            
+            %Si on n'a pas de full ou de double paire, alors c'est
+            %forcément une paire simple
+            nb_simple_paire = nb_simple_paire + 1;
+            break
+        end
+        %Si l'itération arrive jusqu'ici, c'est qu'on est sur un 1 ou un
+        %0. On va donc tester sur on est sur une famille
+        z=0;
+        for d=1 : taille_colors
+            if tab_nb_couleurs(d) == 1
+                z = z+1;
+            end
+        end
+        if z == 5
+            nb_famille = nb_famille + 1;
+            break
+        end
+    end
+end
+
+
+%On peut maintenant calculer les probabilités de chaque main
+p_simple_paire = nb_simple_paire /taille;
+p_double_paire = nb_double_paire /taille;
+p_famille = nb_famille /taille;
+p_brelan = nb_brelan /taille;
+p_full = nb_full /taille;
+p_carre = nb_carre /taille;
+p_couleur = nb_couleur /taille;
+
+%et l'espérance du gain X
+esperance = p_simple_paire*0 + p_double_paire*0.75 + p_famille*0.8 + p_brelan*2 + p_full*4.5 + p_carre*7 + p_couleur*10;
+
+%ainsi que la variance (théorème de König-Huygens)
+variance = p_simple_paire*0*0 + p_double_paire*0.75*0.75 + p_famille*0.8*0.8 + p_brelan*2*2 + p_full*4.5*4.5 + p_carre*7*7 + p_couleur*10*10 - esperance*esperance;
+
+toc
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
